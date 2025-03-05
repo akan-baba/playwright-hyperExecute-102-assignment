2450223801d41ff5b41baaeefff11c18390a4799
akan-baba <akan_antia@live.co.uk>
2025-03-05 19:09:18 +0000

    Final submission for Playwright 102

--- playwright.config.js
+++ playwright.config.js
@@ -8,14 +8,21 @@ export default defineConfig({
   testDir: "./e2e",
   testMatch: "**/*.spec.js",
   timeout: 120000,
+  globalTimeout: 600000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   reporter: "html",
+  
+  
 
   use: {
-    trace: "on-first-retry",
+    trace: "on",
+    video: "retain-on-failure",
+    screenshot: "only-on-failure",
+    console: "on",
+    network: "on",
   },
 
   /* Configure projects for major browsers */
